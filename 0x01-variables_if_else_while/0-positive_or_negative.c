96d5aae402d82645e7db6d044a2c2f01790639c9
Akinlolu <akinloluoladejo@gmail.com>
2023-02-17 12:21:38 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /** betty style doc for function main goes there */
 /**
- * main - Entry point
- * Return: 0
+ * main - prints if the number is positive, negative or 0
+ * Return: Always 0
  */
 int main(void)
 {

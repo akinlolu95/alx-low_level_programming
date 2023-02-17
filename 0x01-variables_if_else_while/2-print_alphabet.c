090314f431843b9757548c3ad818288339472bcb
Akinlolu <akinloluoladejo@gmail.com>
2023-02-17 13:25:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp == 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
